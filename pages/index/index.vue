ad4bdfcbf9d121454e37b72c5e02edf921bb227d
BruceAKABear <dengyi@dengyi.pro>
2020-01-10 22:56:50 +0800

    初始化提交

---
+++ pages/index/index.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="content">
+		<image class="logo" src="../../static/logo.jpg" mode="scaleToFill"></image>
+		<view class="text-area">
+			<text class="title">{{ title }}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: 'keep creative'
+		};
+	},
+	onLoad() {
+		setTimeout(function(){
+			uni.reLaunch({
+				url:'../setting/setting'
+			})
+		},2000)
+		
+	},
+	methods: {}
+};
+</script>
+
+<style lang="scss">
+.content {
+	position: absolute;
+	width: 400rpx;
+	height: 400rpx;
+	
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%,-50%);	
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	text-align:center;
+	.logo {
+		height: 300rpx;
+		width: 400rpx;
+	}
+	.text-area {
+		display: flex;
+		justify-content: center;
+		.title {
+			font-size: 40rpx;
+			color: #8f8f94;
+			text-align: center;
+		}
+	}
+}
+</style>
---
+++ pages/setting/setting.vue
@@ -0,0 +1,74 @@
+<template>
+	<view class="content">
+		<!-- 蓝牙连接信息 -->
+		<view class="content-header">
+			<!-- 蓝牙图标容器 -->
+			<view class="content-header-connectbox">
+				<view class="btinfobox" v-if="btconnected">
+					<image class="bticon" src="../../static/lanya-on.png" mode="scaleToFill"></image>
+					<text>已连接</text>
+				</view>
+				<view class="btinfobox" v-else>
+					<image class="bticon" src="../../static/lanya-off.png" mode="scaleToFill"></image>
+					<text>未连接</text>
+				</view>
+			</view>
+			<!-- 蓝牙设备电量容器 -->
+			<view class="content-header-batterybox" v-if="btconnected">
+				<image class="battery" src="../../static/lanya-off.png" mode="scaleToFill"></image>
+				<text>200%</text>
+			</view>
+		</view>
+		<view class="content-body"></view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			btconnected: true
+		};
+	}
+};
+</script>
+
+<style lang="scss">
+.content {
+	display: flex;
+	flex-direction: column;
+	.content-header {
+		display: flex;
+		position: absolute;
+		top: 30rpx;
+		right: 30rpx;
+		width:25%;
+		.content-header-connectbox {
+			width:50%;
+			.bticon {
+				width: 40rpx;
+				height: 40rpx;
+			}
+			.btinfobox {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				font-size: 25rpx;
+			}
+		}
+		.content-header-batterybox {
+			display: flex;
+			font-size: 25rpx;
+			margin-left: 25rpx;
+			width: 50%;
+			align-items: center;
+			justify-content: center;
+			flex-direction: column;
+			.battery {
+				width: 40rpx;
+				height: 40rpx;
+			}
+		}
+	}
+}
+</style>
